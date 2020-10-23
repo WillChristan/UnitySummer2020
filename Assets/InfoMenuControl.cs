bd614f9d438d6563b7b15ff08eae9cbfa9deb9f1
William Ch <wchristan@gmail.com>
2020-10-23 18:55:42 -0400

    InfoMenuControl

---
+++ Assets/InfoMenuControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoMenuControl : MonoBehaviour
+{
+    public GameObject infoMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(infoMenu != null)
+        {
+            infoMenu.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (infoMenu == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (infoMenu.activeInHierarchy)
+            {
+                infoMenu.SetActive(false);
+            }
+            else
+            {
+                infoMenu.SetActive(true);
+            }
+        }
+
+
+    }
+
+}
